b68a0855aab6f23923ecb502018b5ea0b31d7028
BetterCallAle <dap22091998@gmail.com>
2023-03-23 15:02:15 +0100

    Milestone 2 completed

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -1,21 +1,43 @@
 package org.lessons.java;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     private static final String DB_URL = System.getenv("DB_URL");
     private static final String DB_USERNAME = System.getenv("DB_USERNAME");
     private static final String DB_PASSWORD = System.getenv("DB_PASSWORD");
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Effettua una ricerca per nazione: ");
+        String userChoice = "%";
+        userChoice += scan.nextLine();
+        userChoice += "%";
+        scan.close();
+
+
         try(Connection con = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)){
             String sql = """
-                    SELECT regions.name, countries.country_id, continents.name, countries.name  FROM regions
+                    SELECT countries.name, countries.country_id, regions.name, continents.name  FROM regions
                     JOIN continents on regions.continent_id = continents.continent_id
-                    JOIN countries on regions.region_id = countries.region_id;
+                    JOIN countries on regions.region_id = countries.region_id
+                    WHERE countries.name LIKE ?
+                    ORDER BY countries.name;
                     """;
 
-            try(PreparedStatement ps = con.prepareStatement(sql)) {
+            try(PreparedStatement ps = con.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)) {
+
+                ps.setString(1, userChoice);
+
                 try(ResultSet rs = ps.executeQuery()){
+
+                    if(!rs.next()){
+                        System.out.println("Non ci sono risultati per " + userChoice);
+                    } else {
+                        rs.beforeFirst();
+                    }
+
                     while (rs.next()){
                         String regionName = rs.getString(1);
                         int countryId = rs.getInt(2);
@@ -27,7 +49,7 @@ public class Main {
                 }
             }
         } catch (SQLException e){
-            e.getStackTrace();
+            e.printStackTrace();
         }
     }
 }
